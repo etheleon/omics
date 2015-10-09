e7d7f2974f4b2b78bfb48923ca75a88053102ed6
etheleon <picy2k@gmail.com>
2015-10-09 08:37:01 +0800

    removed DScmdLine-Rio submodule

--- script/make_metabolism.sh
+++ script/make_metabolism.sh
@@ -2,7 +2,7 @@
 
 targetdir=$HOME/db/neo4j
 keggdump=$HOME/KEGG/KEGG_SEPT_2014
-meta4jHome=$HOME/downloads/meta4j
+meta4jHome=$HOME/downloads/meta4j #Weird
 cores="1"
 
 #Initial processing
@@ -27,54 +27,57 @@ cat $targetdir/misc/*_konodes > $targetdir/misc/combined_redundant_konodeslist
 #' combines redundant ko entries into one with pathways as arrays 
 rm -f $targetdir/out/nodes/newkonodes
 
-for i in `perl -aln -F"\t" -e 'print $F[0] unless m/ko:string:koid/' $targetdir/misc/*_konodes | sort | uniq`	#For each ko
+echo "## Processing Metabolic KOs"
+for i in `perl -aln -F"\t" -e 'print $F[0] unless m/ko:ID/' $targetdir/misc/*_konodes | sort | uniq`
 do
-    grep $i $targetdir/misc/combined_redundant_konodeslist | perl -aln -F"\t" -e '$earlier=join("\t",@F[0..3]) if $.==1; push(@pathwayid, $F[4]); push(@pathwayname, $F[5]);END{print qq($earlier\t), join("|", @pathwayid),qq(\t), join("|", @pathwayname)}' >> $targetdir/out/nodes/newkonodes;
+    grep $i $targetdir/misc/combined_redundant_konodeslist | perl -aln -F"\t" -e '$earlier=join("\t",@F[0..3]) if $.==1; push(@pathwayid, $F[4]); push(@pathwayname, $F[5]); END{print qq($earlier\t), join("|", @pathwayid),qq(\t), join("|", @pathwayname)}' >> $targetdir/out/nodes/newkonodes;
 done;
 
-perl -Mautodie -E 'BEGIN{open METABKOS, "<", "$ARGV[0]/out/nodes/newkonodes";while(<METABKOS>){$kos{(split /\t/)[0]}++ unless $. == 1};open METABNODES, ">>", "$ARGV[0]/out/nodes/newkonodes";open NONMETABS, "<", "$ARGV[0]/misc/ko_nodedetails";}while(<NONMETABS>){chomp;say join "$_\tko" unless exists $kos{(split /\t/)[0]}}' $targetdir
-
-perl -0777 -pi -E '@header = qw/ko:ID name definition l:label pathway:string_array pathway.name:string_array/; say(join("\t", @header))' $targetdir/out/nodes/newkonodes #prints the header
+perl -0777 -pi -E '@header = qw/ko:ID name definition l:label pathway pathway.name/; say(join("\t", @header))' $targetdir/out/nodes/newkonodes #prints the header
 perl -pi -e 's/\"/\\"/g' $targetdir/out/nodes/newkonodes	#protects removes
+echo "## Add ko:K00000" #need to add all non-metabolic KOs
+echo -e 'ko:K00000\tUnassigned\tUnassigned\tko' >> $targetdir/out/nodes/newkonodes 
 
-#need to add all non-metabolic KOs
-echo -e 'ko:K00000\tUnassigned\tUnassigned\tko' >> $targetdir/out/nodes/newkonodes	#empty field
-
-#Add in non-metabolic kos
+echo "## Processing Non-Metabolic KOs"
 ./script/kegg/kegg.0400.non-metabolic.pl $targetdir
 
 #Prepare nodes (escape lucene (neo4j's query engine) metacharacters)
 #shd do this for all but lets just let it be first
-perl -pi -e 's/([\+\-\&\|\|\!\(\)\{\}\[\]\^\"\~\*\?\:\\])/\\$1/ unless $. == 1' out/nodes/newkonodes
-#perl -0777 -pi -e 'print qq(ko:string:koid\tname\tdefinition\tl:label\tpathway:string_array\tpathway.name:string_array\n)' nodes/newkonodes
-
-
-#Add in modules
+echo "## Clean for neo4j"
+perl -pi -e 's/([\+\-\&\|\|\!\(\)\{\}\[\]\^\"\~\*\?\:\\])/\\$1/g unless $. == 1' $targetdir/out/nodes/newkonodes
 
 #--CPD
 rm -f $targetdir/misc/newcpdnodes
 cat $targetdir/misc/*_cpdnodes |perl -ne 'print unless /cpd\:string\:cpdid/' | sort | uniq >> $targetdir/out/nodes/newcpdnodes	#removes redundancy
 perl -0777 -pi -e 'print qq(cpd:ID\tname\tl:label\n)' $targetdir/out/nodes/newcpdnodes	
-perl -pi -e 's/\"/\\"/g' $targetdir/out/nodes/newcpdnodes
+perl -pi -e 's/([\+\-\&\|\|\!\(\)\{\}\[\]\^\"\~\*\?\:\\])/\\$1/g unless $. == 1' $targetdir/out/nodes/newcpdnodes
+#perl -pi -e 's/\"/\\"/g' $targetdir/out/nodes/newcpdnodes
 
 
 #--Pathways
 perl -aln -F"\t" -e 'print qq($F[4]\t$F[5]\tpathway) unless $.==1' $targetdir/misc/combined_redundant_konodeslist | sort | uniq > $targetdir/out/nodes/pathwaynodes
 perl -0777 -pi -e 'print qq(pathway:ID\tpathwayname\tl:label\n)' $targetdir/out/nodes/pathwaynodes
+perl -pi -e 's/([\+\-\&\|\|\!\(\)\{\}\[\]\^\"\~\*\?\:\\])/\\$1/g unless $. == 1' $targetdir/out/nodes/pathwaynodes
 
 ##################################################
 #EDGES
 ##################################################
 
 #-- ko2cpd, cpd2ko
-perl -ne 'BEGIN{print qq(ko:START_ID\tcpd:END_ID\trelationship:TYPE\trxnID\n)}print if !/^ko:string/' <(cat $targetdir/misc/*cpd.rels | sort | uniq) > $targetdir/out/rels/newcpdrels
-perl -ne 'BEGIN{print qq(cpd:START_ID\tko:END_ID\trelationship:TYPE\trxnID\n)}print if !/^cpd:string/' <(cat $targetdir/misc/*ko.rels | sort | uniq) > $targetdir/out/rels/newkorels
+echo "## ko2cpd cpd2ko"
+perl -ne 'BEGIN{print qq(ko:START_ID\tcpd:END_ID\trxnID\trelationship:TYPE\n)}print if !/^ko:ID/' <(cat $targetdir/misc/*cpd.rels | sort | uniq) > $targetdir/out/rels/newcpdrels
+perl -pi -e 's/([\+\-\&\|\|\!\(\)\{\}\[\]\^\"\~\*\?\:\\])/\\$1/g unless $. == 1' $targetdir/out/rels/newcpdrels
+perl -ne 'BEGIN{print qq(cpd:START_ID\tko:END_ID\trxnID\trelationship:TYPE\n)}print if !/^cpd:ID/' <(cat $targetdir/misc/*ko.rels | sort | uniq) > $targetdir/out/rels/newkorels
+perl -pi -e 's/([\+\-\&\|\|\!\(\)\{\}\[\]\^\"\~\*\?\:\\])/\\$1/g unless $. == 1' $targetdir/out/rels/newkorels
 
 ##-- pathway2KO
 rm -f $targetdir/out/rels/ko2pathwayrels
 for i in `perl -aln -F"\t" -e 'print $F[0] unless m/ko:ID/' $targetdir/misc/*_konodes | sort | uniq`
 do
-grep $i $targetdir/misc/combined_redundant_konodeslist | perl -aln -F"\t" -e '$ko = $F[0] if $.==1; push(@pathwayid, $F[4]); END{foreach $e (@pathwayid){print qq($ko\t$e\tpathwayed)}}' >> $targetdir/out/rels/ko2pathwayrels;
+    grep $i $targetdir/misc/combined_redundant_konodeslist | perl -aln -F"\t" -e '$ko = $F[0] if $.==1; push(@pathwayid, $F[4]); END{foreach $e (@pathwayid){print qq($ko\t$e\tpathwayed)}}' >> $targetdir/out/rels/ko2pathwayrels;
 done;
+
 perl -0777 -pi -e 'print qq(ko:START_ID\tpathway:END_ID\trelationship:TYPE\n)' $targetdir/out/rels/ko2pathwayrels
+perl -pi -e 's/([\+\-\&\|\|\!\(\)\{\}\[\]\^\"\~\*\?\:\\])/\\$1/g unless $. == 1' $targetdir/out/rels/ko2pathwayrels
+
 script/kegg/kegg.0500.igraphMetabolism.r $targetdir/misc/
