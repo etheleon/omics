a29e2964ce8e950eb1ebf748d5be67e5dc9d4f04
etheleon <picy2k@gmail.com>
2017-03-21 08:39:34 +0800

    Edit > to >> in make_taxonomy

--- script/make_taxonomy.sh
+++ script/make_taxonomy.sh
@@ -37,5 +37,5 @@ echo -e '0\tUnclassified\tno rank|Taxon' >> $targetdir/out/nodes/tax_nodes  #thi
 #1       1       child.of
 ##################################################################
 perl -E 'say qq(taxid:START_ID\ttaxid:END_ID\trelationship:TYPE\tstartend)' > $targetdir/out/rels/tax2tax.rel
-perl -aln -F"\\t\\|\\t" -e 'print qq($F[0]\t$F[1]\tchildof\t$F[0]_$F[1]) unless $F[0] == $F[1]' $taxodump/nodes.dmp > $targetdir/out/rels/tax2tax.rel
+perl -aln -F"\\t\\|\\t" -e 'print qq($F[0]\t$F[1]\tchildof\t$F[0]_$F[1]) unless $F[0] == $F[1]' $taxodump/nodes.dmp >> $targetdir/out/rels/tax2tax.rel
 ##################################################################
